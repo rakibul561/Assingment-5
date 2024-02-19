78b2e17d47528516d248142155b821374c57cc44
rakib561 <rakibulhasan3929@gmail.com>
2024-02-19 22:10:46 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,79 @@
+let count = 40;
+const allSit = document.getElementsByClassName("sit");
+for (const sit of allSit) {
+    sit.addEventListener("click", function () {
+        count = count - 1;
+        if (count >= 0) {
+
+            document.getElementById("sit-count").innerText = count;
+        }
+        else {
+            alert('this buses sit fillap');
+        }
+        // console.log(count)
+    });
+}
+
+let done = 0;
+const sitDone = document.getElementsByClassName("sit");
+for (const sit of sitDone) {
+    sit.addEventListener("click", function (element) {
+        done = done + 1;
+
+        // const selectSit=document.getElementById()
+
+        // const clickSit=document.getElementById('click-sit').innerText=sitName;
+
+        setText("sit-done", done);
+    });
+}
+
+function setText(id, value) {
+    document.getElementById(id).innerText = value;
+}
+
+
+
+
+let sum = 0;
+const allBtn = document.getElementsByClassName('sit');
+for (const btn of allBtn) {
+
+    btn.addEventListener('click', function handler(event) {
+        sum += 1;
+        const sets = event.target.innerText;
+        console.log(sets);
+
+        const types = 'economy';
+        const price = 550;
+        showClassSeat('newSide', sets, types, price);
+
+        if (sum <= 4) {
+            // alert('this sit is not allow');
+            event.target.classList.add('bg-green-400');
+            calTotal('total-cost', price);
+
+            grandTotal('grand-total', price);
+            availableSit('sit-available');
+            event.target.setAttribute("disabled", true);
+        }
+        else {
+            if (sum === 5) {
+                alert('this sit is not allow');
+
+            }
+
+        }
+    })
+}
+
+
+
+
+function start() {
+    HideElementById('home');
+    showElementById('sender');
+
+
+
+}
